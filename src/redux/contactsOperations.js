05afc1324884c213d9e9ddda4e95e494d243e086
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-03-12 18:12:56 +0200

    deleted dublicate code

--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://6402478f302b5d671c37899f.mockapi.io/api/v1';
-
 export const fetchContacts = createAsyncThunk('contacts/fetchAll',
 	async (_, thunkAPI) => {
 		try {
