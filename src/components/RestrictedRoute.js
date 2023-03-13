3c7757e6a880e130986da86c218d772b136a090b
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-03-13 18:03:48 +0200

    fixed RestrictedRoutes

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,7 +1,9 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from 'hooks';
  
-export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
 	const { isLoggedIn } = useAuth();
-  	return isLoggedIn ? <Navigate to={redirectTo} /> : <Component />;
+  	return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
+
+export default RestrictedRoute;
